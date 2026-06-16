3f7cf7f23704102bf8aeac355ed4b0f6ee6c50b0
Felipe Avelino <68569965+FelipeAreosoAvelino@users.noreply.github.com>
2026-06-16 12:29:21 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Pessoas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Forms.MDI_menu());
         }
     }
 }
